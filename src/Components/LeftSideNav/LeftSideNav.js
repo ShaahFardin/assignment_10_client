e64bfb6b632917142e0cd2beef28a923ef4ffd01
shaahfardin <fardinshaah@gmail.com>
2022-10-26 21:51:07 +0600

    Changes in left side nav bar

--- src/Components/LeftSideNav/LeftSideNav.js
+++ src/Components/LeftSideNav/LeftSideNav.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Button, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import './LeftSideNav.css'
+import './LeftSideNav.css'
 
 const LeftSideNav = () => {
   const [catagories, setCatagories, ] = useState([]);
@@ -15,21 +16,31 @@ const LeftSideNav = () => {
   return (
     <div className="Nav d-none d-lg-block">
       <div style={{ position: "fixed", top: "10" }}>
-        <h3 >Our courses</h3>
-        {catagories.map((catagory) => (
+        <h3>Our courses</h3>
+        {/* {catagories.map((catagory) => (
           <p className="mt-3" key={catagory.catagory_id}>
-            <ListGroup>
+            <ListGroup >
               <Button variant="light">
                 <Link
-                  style={{textDecoration: "none", color:"black"}}
+                  
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    
+                  }}
                   to={`/catagory/${catagory.catagory_id}`}
                 >
                   {" "}
-                  {catagory.catagory_name}
+                  <li>{catagory.catagory_name}</li>
                 </Link>
               </Button>
             </ListGroup>
           </p>
+        ))} */}
+        {catagories.map((catagory) => (
+          <Link to={`/catagory/${catagory.catagory_id}`} className="linkStyle">
+            <p className="allCourses">{catagory.catagory_name}</p>
+          </Link>
         ))}
       </div>
     </div>
